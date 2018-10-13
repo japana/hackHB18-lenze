52818134a261cd54f7aedb841e25605f7b76c079
jpn <jp.nachtwey@gmail.com>
2018-10-13 16:15:15 +0200

    added OMG Decoder

--- OpticalMessageGuide/app/src/test/java/io/omg/opticalmessageguide/ExampleUnitTest.java
+++ OpticalMessageGuide/app/src/test/java/io/omg/opticalmessageguide/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package io.omg.opticalmessageguide;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+
+import io.omg.opticalmessageguide.streamprocessor.OMGDecoder;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testEncoder() throws Exception {
+
+        String expected = "Hello world";
+
+        byte[] trash = "bbbbbbb".getBytes();
+        byte[] header_footer = {OMGDecoder.DIVIDER};
+        byte[] helloWorld = expected.getBytes();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        os.write(trash);
+        os.write(header_footer);
+        os.write(helloWorld);
+        os.write(header_footer);
+        os.write(trash);
+
+        OMGDecoder encoder = new OMGDecoder();
+        encoder.getOutputStream().write(os.toByteArray());
+        Thread.sleep(1000);
+        String value = encoder.getMsg();
+        encoder.close();
+
+
+
+        Assert.assertEquals("whatever", expected, value);
+
     }
+
 }
