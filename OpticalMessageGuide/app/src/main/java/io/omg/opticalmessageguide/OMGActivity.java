aa15a4bd2bb1ae80d1b54180b675af46d9957a02
Lars Oetermann <lars-oetermann@gmx.de>
2018-10-13 02:27:35 +0200

    project created

---
+++ OpticalMessageGuide/app/src/main/java/io/omg/opticalmessageguide/OMGActivity.java
@@ -0,0 +1,13 @@
+package io.omg.opticalmessageguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OMGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_omg);
+    }
+}
